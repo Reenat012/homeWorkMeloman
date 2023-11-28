2dccc7efbb9af3505b7a606ab76f472e9babd716
Reenat <reenat056@yandex.ru>
2023-11-28 21:29:49 +0300

    make edits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 fun main() {
-    val buy = 1000 //сумма покупки без учета скидки
+    val buy = 1001 //сумма покупки без учета скидки
     val regularCustomer = true //регулярный покупатель или нет
     //вычисляем cумму покупки со скидкой, но без учета постоянства
-    val buyWithDicount = if (buy > 0 && 1000 <= buy) buy
+    val buyWithDicount = if (buy > 0 && 1000 >= buy) buy
                     else if (1000 < buy && 10_000 >= buy) buy - 100
-                    else buy - (buy * 0.05)
+                    else buy - (buy * 0.05).toInt()
     //вычисляем скидку с учетом постоянства
     val buyWithRegular = if (regularCustomer == true) buyWithDicount - (buyWithDicount * 0.01)
                     else buyWithDicount
+    println(buyWithRegular)
 }
 
